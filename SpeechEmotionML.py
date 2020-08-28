f8d186796f46ada2d1827ac2fd443c0cd1591ff1
LykaiosOS <lykaiosos@gmail.com>
2020-08-28 15:04:23 -0700

    Importing packages

--- SpeechEmotionML.py
+++ SpeechEmotionML.py
@@ -1,1 +1,15 @@
 
+## Python
+import os
+import sys
+import random
+
+## Packages
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as mplt
+import tensorflow as tf
+import librosa
+import librosa.display
+import scipy
+
